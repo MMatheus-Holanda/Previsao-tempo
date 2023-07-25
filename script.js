b6a33faeeb28c43be9213c1d39861657bc0c526d
Matheus Holanda <matheusholanda0997@gmail.com>
2023-07-25 17:52:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const key = "fd227e4680fab2153d7856cb31bbe4e4"
+
+
+function colocarDadosTela(dados) {
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".icone").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function buscarCidade(cidade) {
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
+    
+    colocarDadosTela(dados)
+}
+
+function cliqueiNoBotao() {
+    const cidade = document.querySelector(".busca").value  
+  
+    buscarCidade(cidade)
+}
+
+
